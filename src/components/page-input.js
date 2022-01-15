33ae0ca3d481651b5eb3343bc2a3c17bb3dde61e
tongong <tongong@gmx.net>
2022-01-15 21:58:08 +0100

    added about text

--- src/components/page-input.js
+++ src/components/page-input.js
@@ -3,11 +3,11 @@ const input = require("../modules/input.js");
 const keyBoxes = require("./key-boxes.js");
 
 module.exports = () => {
-    let written = "> ";
+    let written = "";
     let blind = false;
     let oninput = (c) => {
         // clear input on "-" -> new bullet point / new thought
-        if (c == "-") written = "> ";
+        if (c == "-") written = "";
         else if (c == input.deletechar)
             written = written.substring(0, written.length - 1)
         else written += c;
@@ -18,7 +18,7 @@ module.exports = () => {
     };
     return {
         view: () => m("div",
-            m("div.inputTop", written),
+            m("div.inputTop", "> " + written),
             m("div.inputBottom", m(keyBoxes, {
                 inputCallback: oninput,
                 keyCallback: blindhandler
